bc060c668582eb7295d98247c2602b8a1ba97b91
Daoren <daorenfeixueyuhua@163.com>
2019-10-24 09:46:48 +0800

    third commit

--- src/main/java/com/example/day04/file/First.java
+++ src/main/java/com/example/day04/file/First.java
@@ -6,6 +6,15 @@ import java.io.File;
 
 public class First {
     public static void main(String[] args) {
+        // 日你
+
         File file = new File("/text.txt");
+        /**
+         * Hello
+         */
+        {
+            System.out.println("hello");
+        }
+//        System.out.println("草");
     }
 }
